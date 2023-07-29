fd367d8947f6825ea49582df878411ff6e577b88
jungbam <jungbam0809@gmail.com>
2023-07-30 01:46:40 +0900

    [FEAT] next.js config를 통한 redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["plus.unsplash.com", "images.unsplash.com", "unsplash.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className={inter.className}>
         <header className={styles.header}>
           <h1>Demo Note App</h1>
